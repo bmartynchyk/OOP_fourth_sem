8769cf02be4b04658137e3295feeb2e8bed36d4a
Thibyls Emory <bmartynchyk@gmail.com>
2019-04-01 22:43:31 +0300

    implemented exracting pairs form file

--- OOP_labs/OOP_lab_1/Dictionary.cpp
+++ OOP_labs/OOP_lab_1/Dictionary.cpp
@@ -1,7 +1,10 @@
 
 #include <iostream>
+#include <fstream>
 #include <list>
+
 #include "Translate.h"
+#define WSIZE 32
 
 using namespace std;
 
@@ -14,6 +17,25 @@ void SortByIta(list<CTranslate> &lst);
 void TranslateEng(const list<CTranslate> &lst, const char *eng);
 void TranslateIta(const list<CTranslate> &lst, const char *ita);
 
-void main() {
+int main() {
+	ifstream eng_ita("English-Italian.txt");
+	list<CTranslate> lst;
+	CTranslate tmp;
+	char eng[WSIZE], ital[WSIZE];
 
+	// Pushing to the list all pairs from file. Pairs should be plased is such way: 
+	//<1st eng word>;<1st ital word>;<2nd eng word>;<2nd ital word>;<3rd ...
+	while (!eng_ita.eof()) {
+		
+		eng_ita.getline(eng, WSIZE, ';');
+		eng_ita.getline(ital, WSIZE, ';');
+
+		tmp.MakePair(eng, ital);
+		cout << eng << " - " << ital << endl;
+
+		lst.push_back(tmp);
+	}
+
+	eng_ita.close();
+	system("pause");
 }
